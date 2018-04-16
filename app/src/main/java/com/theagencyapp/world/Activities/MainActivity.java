5513b068f3761a0f7801e7c015cb3fdec0409257
Abdul Hameed Azeemi <maxer232@gmail.com>
2018-04-16 21:36:07 +0500

    Added Bottom Navigation
    Signed-off-by: Abdul Hameed Azeemi <maxer232@gmail.com>

--- app/src/main/java/com/theagencyapp/world/Activities/MainActivity.java
+++ app/src/main/java/com/theagencyapp/world/Activities/MainActivity.java
@@ -2,9 +2,12 @@ package com.theagencyapp.world.Activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,11 +18,42 @@ public class MainActivity extends AppCompatActivity {
     FirebaseAuth auth;
     FirebaseAuth.AuthStateListener authListener;
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_projects:
+                    mTextMessage.setText(R.string.title_projects);
+                    return true;
+                case R.id.navigation_teams:
+                    mTextMessage.setText(R.string.title_teams);
+                    return true;
+                case R.id.navigation_clients:
+                    mTextMessage.setText(R.string.title_clients);
+                    return true;
+                case R.id.navigation_chat:
+                    mTextMessage.setText(R.string.title_chat);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         auth=FirebaseAuth.getInstance();
         authListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -33,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
     }
 
     public void onLogout(View v)
