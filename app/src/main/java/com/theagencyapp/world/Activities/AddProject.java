9eb5c3d3885e83b966b2748bb4e054e16b26dbb0
Abdul Hameed Azeemi <maxer232@gmail.com>
2018-04-17 17:39:33 +0500

    Designed Add Project Screen
    Signed-off-by: Abdul Hameed Azeemi <maxer232@gmail.com>

--- app/src/main/java/com/theagencyapp/world/Activities/AddProject.java
+++ app/src/main/java/com/theagencyapp/world/Activities/AddProject.java
@@ -1,36 +1,63 @@
 package com.theagencyapp.world.Activities;
 
 import android.app.DatePickerDialog;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import com.theagencyapp.world.R;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
 
-public class AddProject extends AppCompatActivity {
+public class AddProject extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private EditText edittext;
     final Calendar myCalendar = Calendar.getInstance();
+    ImageButton high;
+    ImageButton medium;
+    ImageButton low;
+
+    String priority = null;
+    int clientSelected = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_project);
 
 
-        edittext = (EditText) findViewById(R.id.add_project_deadline);
+        edittext = findViewById(R.id.add_project_deadline);
+        high = findViewById(R.id.priority_high);
+        medium = findViewById(R.id.priority_medium);
+        low = findViewById(R.id.priority_low);
+        Spinner sp = findViewById(R.id.clients_spinner);
+
+        ArrayList<String> data = new ArrayList<>();
+        data.add("No Client");
+        data.add("Textra Software Solutions");
+        data.add("Netsoft Solutions");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, data);
+        sp.setAdapter(adapter);
+
+        sp.setOnItemSelectedListener(this);
+
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
-                // TODO Auto-generated method stub
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -43,7 +70,6 @@ public class AddProject extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 new DatePickerDialog(AddProject.this, date, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
@@ -56,5 +82,46 @@ public class AddProject extends AppCompatActivity {
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
         edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    public void onHighPriorityClick(View view) {
+        high.setImageResource(R.drawable.fire_checked);
+        medium.setImageResource(R.drawable.drop);
+        low.setImageResource(R.drawable.leaf);
+        priority = "high";
+    }
+
+    public void onMediumPriorityClick(View view) {
+        high.setImageResource(R.drawable.fire);
+        medium.setImageResource(R.drawable.drop_checked);
+        low.setImageResource(R.drawable.leaf);
+        priority = "medium";
+    }
+
+    public void onLowPriorityClick(View view) {
+        high.setImageResource(R.drawable.fire);
+        medium.setImageResource(R.drawable.drop);
+        low.setImageResource(R.drawable.leaf_checked);
+        priority = "low";
+    }
+
+    public void onDoneClick(View view) {
+        if (priority == null) {
+            Snackbar snackbar = Snackbar
+                    .make(findViewById(R.id.main_layout_id), "No Priority Selected!", Snackbar.LENGTH_LONG);
+
+            snackbar.show();
+            return;
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        this.clientSelected = i;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
